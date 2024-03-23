0a1741fc0353749105a6678de5e4948561080286
Zackary Ayoun <zackaryayoun@gmail.com>
2024-03-23 19:41:49 +0200

    strace-rs (#1)

--- TODO.md
+++ TODO.md
@@ -2,20 +2,24 @@
 
 ## tools
 
-* strace
-* sscov
-* addr2line
-* gcov
-* ltrace
-* mmck
-* dbg
-* ld
-* ld.so
-* ...
+[x] strace  
+&nbsp;&nbsp;&nbsp;&nbsp;[x] handle signals  
+&nbsp;&nbsp;&nbsp;&nbsp;[ ] add missing syscalls in syscall/info.json  
+&nbsp;&nbsp;&nbsp;&nbsp;[ ] follow child on fork  
+&nbsp;&nbsp;&nbsp;&nbsp;[ ] syscall filtering  
+[ ] sscov  
+[ ] addr2line  
+[ ] gcov  
+[ ] ltrace  
+[ ] mmck  
+[ ] dbg  
+[ ] ld  
+[ ] ld.so  
+[ ] ...  
 
 ## infra
 
-* testing
-* coding style
-* proper README
-* support more architectures
+[x] apply clippy::pedantic lints  
+[ ] testing  
+[ ] proper README  
+[ ] support more architectures  
---
+++ src/syscall/info.rs
@@ -0,0 +1,45 @@
+use serde_derive::Deserialize;
+use std::collections::HashMap;
+
+use crate::diag::Result;
+
+#[derive(Clone, Deserialize)]
+pub enum Type {
+    Int,
+    Ptr,
+    Str,
+    Uint,
+}
+
+#[derive(Clone, Deserialize)]
+pub struct Arg {
+    pub name: String,
+    pub arg_type: Type,
+}
+
+#[derive(Clone, Deserialize)]
+pub struct Entry {
+    pub name: String,
+    pub ret_type: Type,
+    pub args: Option<Vec<Arg>>,
+}
+
+pub struct Entries {
+    map: HashMap<u64, Entry>,
+}
+
+impl Entries {
+    pub fn new() -> Result<Self> {
+        let json = include_str!("info.json");
+        let map = serde_json::from_str(json)?;
+        Ok(Self { map })
+    }
+
+    pub fn get(&self, id: u64) -> Entry {
+        self.map.get(&id).cloned().unwrap_or_else(|| Entry {
+            name: "unknown".to_string(),
+            ret_type: Type::Int,
+            args: None,
+        })
+    }
+}
