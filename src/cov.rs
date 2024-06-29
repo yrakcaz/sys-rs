e40fc2b8cd45864ade531ca779c9c78b5c50f3ae
Zackary Ayoun <zackaryayoun@gmail.com>
2024-06-29 20:40:51 +0000

    WIP: Fixed most FIXMEs. Just two simple ones left.

--- src/cov.rs
+++ src/cov.rs
@@ -1,4 +1,3 @@
-use goblin::elf;
 use nix::{
     errno::Errno,
     sys::{
@@ -17,24 +16,22 @@ use crate::{
     asm, breakpoint,
     debug::{Dwarf, LineInfo},
     diag::{Error, Result},
-    exec::{get_mem_range, Elf},
+    process,
     trace::terminated,
 };
 
 pub struct Tracer {
     path: String,
-    elf: Elf,
     parser: asm::Parser,
 }
 
 impl Tracer {
     /// # Errors
     ///
-    /// Will return `Err` upon failure to build `exec::Elf` or `asm::Parser`.
+    /// Will return `Err` upon failure to build `asm::Parser`.
     pub fn new(path: &str) -> Result<Self> {
         Ok(Self {
             path: path.to_string(),
-            elf: Elf::build(path)?,
             parser: asm::Parser::new()?,
         })
     }
@@ -42,39 +39,29 @@ impl Tracer {
     pub fn path(&self) -> &str {
         &self.path
     }
-
-    #[must_use]
-    pub fn elf(&self) -> &Elf {
-        &self.elf
-    }
 }
 
-// FIXME this version seems to be working on EXEC/DYN with gdwarf4, but not Rust binaries.
 // FIXME we should check what dwarf version we support and 1) update readme, 2) fail if wrong version.
 
 /// # Errors
 ///
-/// Will return `Err` upon any failure related to parsing ELF or DWARF format,
-/// as well as issues related to syscalls usage (e.g. ptrace, wait).
-fn trace_with<F>(context: &Tracer, child: Pid, mut print: F) -> Result<()>
+/// Will return `Err` upon any failure related to parsing ELF or DWARF format, as
+/// well as issues related to syscalls usage (e.g. ptrace, wait).
+fn trace_with<F>(
+    context: &Tracer,
+    process: &process::Info,
+    child: Pid,
+    mut print: F,
+) -> Result<()>
 where
-    F: FnMut(&asm::Instruction, u64) -> Result<()>,
+    F: FnMut(&asm::Instruction) -> Result<()>,
 {
     let mut breakpoint_mgr = breakpoint::Manager::new(child);
 
     let mut startup_complete = false;
     let mut last_instruction: Option<asm::Instruction> = None;
 
-    wait()?;
-    // FIXME we need a better way to discard unsupported types and all
-    // FIXME and this should somehow be hidden in the elf with the rest of the PIE logic...
-    let offset = if context.elf.etype() == elf::header::ET_DYN {
-        get_mem_range(child, context.path())?.start
-    } else {
-        0
-    };
-
-    breakpoint_mgr.set_breakpoint(context.elf.entry(offset))?;
+    breakpoint_mgr.set_breakpoint(process.entry())?;
 
     ptrace::cont(child, None)?;
     loop {
@@ -86,11 +73,11 @@ where
 
                 let rip = regs.rip;
                 if let Some(opcode) =
-                    context.elf.get_opcode_from_section(rip, ".text", offset)?
+                    process.get_opcode_from_section(rip, ".text")?
                 {
                     let instruction =
                         context.parser.get_instruction_from(opcode, rip)?;
-                    print(&instruction, offset - context.elf.load().unwrap_or(0))?; // FIXME avoid unwrap_or here..
+                    print(&instruction)?;
                     last_instruction = Some(instruction);
                 } else if let Some(instruction) = last_instruction.as_ref() {
                     if instruction.is_call() {
@@ -105,7 +92,7 @@ where
                         //
                         // FIXME Not sure whether the code below is actually ran?]
                         //       Offset doesn't change anything...
-                        if context.elf.is_addr_in_section(ret, ".text", offset)
+                        if process.is_addr_in_section(ret, ".text")
                             && startup_complete
                         {
                             breakpoint_mgr.set_breakpoint(ret)?;
@@ -132,7 +119,8 @@ where
 ///
 /// Will return `Err` upon `trace_with` failure.
 pub fn trace_with_simple_print(context: &Tracer, child: Pid) -> Result<()> {
-    trace_with(context, child, |instruction, offset| {
+    let process = process::Info::build(context.path(), child)?;
+    trace_with(context, &process, child, |instruction| {
         println!("{instruction}");
         Ok(())
     })
@@ -169,11 +157,12 @@ impl Cached {
     ///
     /// Will return `Err` upon `trace_with` failure.
     pub fn trace(&mut self, context: &Tracer, child: Pid) -> Result<()> {
-        let dwarf = Dwarf::build(context.elf())?;
-        trace_with(context, child, |instruction, offset| {
+        let process = process::Info::build(context.path(), child)?;
+        let dwarf = Dwarf::build(&process)?;
+        trace_with(context, &process, child, |instruction| {
             let addr = instruction.addr();
             if let Entry::Vacant(_) = self.cache.entry(addr) {
-                let info = dwarf.addr2line(addr - offset)?;
+                let info = dwarf.addr2line(addr)?;
                 self.cache.insert(addr, info);
             }
 
@@ -182,11 +171,11 @@ impl Cached {
                 .get(&addr)
                 .ok_or_else(|| Error::from(Errno::ENODATA))?
             {
-                if Path::new(&line.path()).exists() { // FIXME really this way?
+                if Path::new(&line.path()).exists() {
                     let key = (line.path(), line.line());
                     *self.coverage.entry(key).or_insert(0) += 1;
                     self.files.insert(line.path());
-                    println!("{line}"); // FIXME this prints offset with PIE. should it?
+                    println!("{line}");
                 }
             }
 
